f9d3ddd0a106d7025d9298b5416c68db3f1a78f0
hosam95 <hosamsaeid95@gmail.com>
2025-01-14 05:12:35 +0200

    migrate to the binary communication protocol & implement the  "CheckWeaponId" method and WeaponIds enum

--- entities/wepon.go
+++ entities/wepon.go
@@ -1,14 +1,30 @@
 package entities
 
-type WeponId int
+import "online-game/msgs"
 
-type Wepon interface {
-	Id() WeponId
+type WeaponId uint8
+
+type Weapon interface {
+	Id() WeaponId
 	Name() string
 	GetCooldown() int
 	GetCooldownLeft() float64
-	OnPress(game *Game, player *Player, data map[string]interface{}) (map[string]interface{}, error)
-	Update(game *Game, player *Player, data map[string]interface{}) (map[string]interface{}, error)
-	OnRelease(game *Game, player *Player, data map[string]interface{}) (map[string]interface{}, error)
+	OnWeaponDown(game *Game, player *Player, data map[string]interface{}) (map[string]interface{}, error)
+	OnWeaponUpdate(game *Game, player *Player, data map[string]interface{}) (map[string]interface{}, error)
+	OnWeaponUp(game *Game, player *Player, data map[string]interface{}) (map[string]interface{}, error)
+	ParseWeaponDownMessage(message msgs.GenericMessage) (map[string]interface{}, bool)
+	ParseWeaponUpMessage(message msgs.GenericMessage) (map[string]interface{}, bool)
+	ParseWeaponUpdateMessage(message msgs.GenericMessage) (map[string]interface{}, bool)
 	Stringify() map[string]interface{}
 }
+
+func CheckWeaponId(id WeaponId, buf []byte) ([]byte, bool) {
+	if len(buf) < 1 {
+		return buf, false
+	}
+	return buf[1:], buf[0] == uint8(id)
+}
+
+const (
+	GrenadeId WeaponId = iota
+)
